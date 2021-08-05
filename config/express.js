29434f9835cfaff36f43c17dd9fcffe819a20321
ebssgd <iamebssgd@yahoo.com>
2021-08-04 20:01:16 -0500

    Finished routes

--- config/express.js
+++ config/express.js
@@ -1,13 +1,34 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
 
 module.exports = (app) => {
-    
-    //TODO: Setup the view engine
+  //   // View Engine Setup
 
-    //TODO: Setup the body parser
+  // app.set('views', path.join(__dirname, 'views')); // setting folder for public files
 
-    //TODO: Setup the static files
+  // // register the partials, hint if it says module not found after you do this, its because the module most likely isn't there! Import it!
 
+  // hbs.registerPartials(__dirname + '/views/partials');
+
+  // // setting view engine to hbs, engine compiles views and data into HTML
+
+  // app.set('view engine', 'hbs');
+  //TODO: Set up the views and the engine
+  app.set("view engine", "hbs");
+  app.engine(
+    "hbs",
+    exphbs({
+      extname: "hbs",
+      defaultLayout: "",
+      layoutsDir: __dirname + "/views",
+      partialsDir: __dirname + "/views",
+    })
+  );
+
+  //TODO: Setup the body parser
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  //TODO: Set up the static files
+  app.use(express.static("static"));
 };
--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,24 @@
 // TODO: Require Controllers...
+const express = require("express");
 
 module.exports = (app) => {
-    // TODO...
+  app.get("/", function (req, res) {
+    res.render("index");
+  });
+  app.get("/details/:id", function (req, res) {
+    res.render("details");
+  });
+  app.get("/create", function (req, res) {
+    res.render("create");
+  });
+  app.post("/create", function (req, res) {
+    console.log("POSTing new body of code. ", req.body);
+    res.send("Form Submitted");
+  });
+  app.get("/about", function (req, res) {
+    res.render("about");
+  });
+  app.get("/*", function (req, res) {
+    res.render("404");
+  });
 };
